4f4e9b69b8ff8254e43388360360d7674a6aee4e
Valery1306 <valerypamplona13@gmail.com>
2023-07-14 16:31:46 -0500

    Comit inicial proyecto

---
+++ app/src/main/java/com/valery1306/myapp2687390/Ciclos.kt
@@ -0,0 +1,133 @@
+package com.valery1306.myapp2687390
+
+class Ciclos {
+}
+fun main() {
+
+    /*    for (i in 1..10) {
+        if ((i%2) !=0) {
+            println("Su numero es impar  $i" )
+        }else{
+            println("Su numero es par  $i")
+        }
+
+    }
+     var cont = 0
+    var impares = 0
+    var cont2 = 0
+    var pares = 0
+    var suma = 0
+    var suma1 = 0
+    for (numero in 1..5) {
+        println("Ingrese el numero ")
+        var numero: Int = readLine()!!.toInt()
+        if (numero <= 0) {
+            break
+        } else {
+            if ((numero % 2) != 0) {
+                println("Su numero es impar $numero")
+                cont = cont + 1
+                impares = cont
+                suma = suma + numero
+
+            } else {
+                println("Su numero es par $numero")
+                cont2 = cont2 + 1
+                pares = cont2
+                suma1 = suma1 + numero
+
+            }
+
+        }
+
+    }
+    println("Tiene $pares numeros pares")
+    println("Tiene $impares numeros impares")
+    println("La suma de los numeros impares es $suma")
+    println("La suma de los numeros pares es $suma1")
+}
+
+ var cont = 0
+    var impares = 0
+    var cont2 = 0
+    var pares = 0
+    var suma = 0
+    var suma1 = 0
+    var inicio=1
+
+    while (inicio==1){
+        println("Ingrese el numero ")
+        var numero: Int = readLine()!!.toInt()
+        if (numero <= 0) {
+            break
+        } else {
+            if ((numero % 2) != 0) {
+                println("Su numero es impar $numero")
+                cont = cont + 1
+                impares = cont
+                suma = suma + numero
+
+            } else {
+                println("Su numero es par $numero")
+                cont2 = cont2 + 1
+                pares = cont2
+                suma1 = suma1 + numero
+
+            }
+
+        }
+    println("Desea ingresar otro numero? 1.si  2.no gracias")
+        inicio= readLine()!!.toInt()
+    }
+    println("Tiene $pares numeros pares")
+    println("Tiene $impares numeros impares")
+    println("La suma de los numeros impares es $suma")
+    println("La suma de los numeros pares es $suma1")
+
+
+    */
+
+    var cont = 0
+    var impares = 0
+    var cont2 = 0
+    var pares = 0
+    var suma = 0
+    var suma1 = 0
+    var inicio=1
+
+    do {
+        println("Ingrese el numero ")
+        var numero: Int = readLine()!!.toInt()
+    } while (inicio==1)
+        if (numero <= 0) {
+            break
+        } else {
+            if ((numero % 2) != 0) {
+                println("Su numero es impar $numero")
+                cont = cont + 1
+                impares = cont
+                suma = suma + numero
+
+            } else {
+                println("Su numero es par $numero")
+                cont2 = cont2 + 1
+                pares = cont2
+                suma1 = suma1 + numero
+
+            }
+
+        }
+        println("Desea ingresar otro numero? 1.si  2.no gracias")
+        inicio= readLine()!!.toInt()
+
+    println("Tiene $pares numeros pares")
+    println("Tiene $impares numeros impares")
+    println("La suma de los numeros impares es $suma")
+    println("La suma de los numeros pares es $suma1")
+
+
+}
+
+
+
+
