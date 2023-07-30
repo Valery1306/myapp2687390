12d6a25f11bb16a4a6753c217ebe9b5a4f11cbf9
Valery1306 <valerypamplona13@gmail.com>
2023-07-30 15:18:17 -0500

    Modificaciones y Reto Nequi terminado

--- app/src/main/java/com/valery1306/myapp2687390/Nequi.kt
+++ app/src/main/java/com/valery1306/myapp2687390/Nequi.kt
@@ -4,25 +4,130 @@ class Nequi {
 }
 
 fun main() {
-    val saldodisponible:Int=4500
-    do {  println("Bienvenid@ a Nequi")
-        println("Ingrese su número de celular")
-        val numero:Int= readLine()!!.toInt()
+    fun inicio():Boolean {
+        var ingresar:Boolean=true
+        var rep=1
 
-        println("Ingrese su clave !Recuerde que debe ser de 4 dígitos!")
-        val clave:Int= readLine()!!.toInt()
-        if (numero==30068876 && clave==1234){
-            println("Bienenido a la plataforma")
-            println("Tiene un saldo disponible de ${saldodisponible}")
+        println("Bienvenid@")
+        while (rep==1) {
+            println("ingrese telefono ")
+            var num = readln()!!.toInt()
+            println("ingrese contraseña (4 digitos)")
+            var con = readln()!!.toInt()
 
-        }else{
-            println("¡Upps! Parece que tus datos de acceso no son correctos, Tienes tres intentos más ")
+            if (num==30068876 && con==1234){
+                println("entra")
+                ingresar=true
+                break
+            }
+            else {
+                println("Sus datos son incorrectos, por favor vuelva a intentarlo")
+                ingresar=false
+            }
         }
+        return ingresar
+    }
 
-        println("Desea continuar 1.siguiente 2.terminar")
-        val op1: Int = readLine()!!.toInt()
+    fun op1(plata:Int): Int {
+        println("tu dinero actual es ${plata}")
+        println("escriba el monto a retirar")
+        var restplata = readln()!!.toInt()
 
-    } while (op1==1)
+        if (restplata > plata) {
+            println("ups, no tienes tanto dinero")
+        } else {
+            return plata-restplata
+            println(plata)
+        }
+        return plata
+    }
+
+    fun op2(plata: Int): Int {
+        println("escribe el número de contacto que va a enviar el dinero")
+        val numero = readLine()!!.toInt()
+        println("tu monto actual es de ${plata} pesos")
+
+        println("digita la cantidad a enviar")
+        val sendmoney = readLine()!!.toInt()
+
+        if (sendmoney > plata) {
+            println("ups, no tienes tanto dinero")
+        } else {
+            println("dinero enviado")
+            return plata - sendmoney
+        }
+        return plata
+    }
+
+
+
+    fun op3(plata: Int): Int{
+        println("digita la cantidad a recargar")
+        val sendmoney = readln()!!.toInt()
+        println("esta seguro de recargar?? true/false")
+        val op = readln()!!.toBoolean()
+        if (op==false){
+            println("proceso cancelado")
+        }
+        else{
+            println("dinero recargado")
+            return plata+sendmoney
+        }
+        return plata
+    }
+
+
+
+
+    fun main() {
+        var entra = inicio()
+        var plata = 4500
+        var again = 1
+        var contactos=arrayListOf("diego","adriana","laura","juan")
+
+
+
+        if (entra == true) {
+            while (again == 1) {
+                println("Bienvenido a Nequi")
+                println("su saldo disponible es de ${plata}")
+                println("Opciones que desea realizar:")
+                println("1. Saca plata")
+                println("2. Envia plata")
+                println("3. Recarga Plata")
+                println("4  Salir del app")
+                println("5. Consultar Saldo")
+                println("6. ver tus contactos")
+                var ele = readln()!!.toInt()
+
+                if (ele==1){
+                    plata=op1(plata)
+                }
+                else if (ele==2){
+                    plata=op2(plata)
+
+                }
+                else if (ele==3){
+                    plata=op3(plata)
+                }
+                else if (ele==4){
+                    println("Salio del aplicativo, vuelva a ingresar")
+                    break
+                }
+                else if (ele==5){
+                    println("Saldo actual de ${plata}")
+                }
+                else if (ele==6){
+                    println("contactos:")
+                    println("----------")
+                    println(contactos)
+
+                }
+                println("desea realizar otro proceso? 1.si  2.no")
+                again = readln()!!.toInt()
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/valery1306/myapp2687390/Reto3.kt
+++ app/src/main/java/com/valery1306/myapp2687390/Reto3.kt
@@ -5,25 +5,79 @@ class Reto3 {
 
 fun main() {
 
-    val autos = mutableListOf<String>()
-    do {
-        println("Bienvenid@")
-        println("Elija que desea realizar 1.RegistrarA 2.MostrarA  3.BuscarA  4.ModificarA  5.EliminarA 6.BorrarLista")
-        val op: Int = readLine()!!.toInt()
-        if (op == 1) {
-            println("Ingrese el auto")
-            val auto: String = readLine()!!.toString()
-            if (autos.contains("auto") == false) {
-                autos.add(auto)
-                println(autos)
+    var op: Int=1
+    val autos= mutableListOf("twingo","chevrolet","ford")
+
+    println("Bienvenid@")
+    while (op==1) {
+        println("1. RegistrarAutos")
+        println("2. Mostrarlista")
+        println("3. BuscarAutos")
+        println("4. ModificarAutos")
+        println("5. EliminarAutos")
+        println("6. EliminarAutos")
+
+        print("Ingrese que proceso desea realizar: ")
+        var op1 = readln()!!.toInt()
+
+        if (op1 == 1) {
+            println("Ingrese el nombre del Auto")
+            var newcar = readLine()
+
+            if(autos.contains(newcar)==false){
+                autos.add(newcar.toString())
+                println(autos)}
+            else if(autos.contains(newcar)==true) {
+                println("el auto ya existe, vuelva a ingresar un auto nuevo")
             }
-        } else {
-            println("El auto ya fue registrado")
-            println("Ingrese el auto")
+
+
+        } else if (op1 == 2) {
+            for ((index, element) in autos.withIndex())
+                println("Auto número ${index} es ${element}")
+
+
+        } else if (op1 ==3 ) {
+            println("Ingrese el nombre del Auto que desea buscar")
+            var searchcar = readLine()
+            var veri = println("${autos.contains(searchcar.toString())}")
+
+
+        } else if (op1 ==4 ) {
+            for ((index, element) in autos.withIndex()){
+                println("Auto numero ${index} es ${element}")}
+
+            println("Ingrese el número del auto que deseas modificar")
+            var eleccmodify = readln()!!.toInt()
+
+            println("Ingresa el nuevo nombre")
+            var eleccmodify2 = readln()!!.toString()
+
+            autos[eleccmodify]=eleccmodify2
+
+            println(autos)
+
+        } else if (op1 == 5) {
+            for ((index, element) in autos.withIndex()){
+                println("Auto numero ${index} es ${element}")}
+
+            println("ingrese el número del Auto que deseas eliminar")
+            var eleceliminar = readln()!!.toInt()
+            autos.removeAt(eleceliminar)
+
+            for ((index, element) in autos.withIndex()){
+                println("Auto número ${index} es ${element}")}
+
+        } else if (op1 == 6) {
+            println("Se eliminara toda la lista de Autos, desea continuar?")
+            println("1. si 2. no")
+            var eliminarlist = readln()!!.toInt()
+            if (eliminarlist== 1) { autos.clear() }
         }
-        println("Desea continuar 1.siguiente 2.terminar")
-        val op1: Int = readLine()!!.toInt()
-    } while (op1 == 1)
+
+        println("desea hacer otro proceso? 1 si 2 no")
+        op = readLine()!!.toInt()
+    }
 }
 
 
